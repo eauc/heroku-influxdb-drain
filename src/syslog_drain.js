bdae38dfacb24db0ae6ac12a1a74561274d98600
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2019-04-01 13:14:01 +0200

    Cleanup tags/fields for heroku logs

--- src/syslog_drain.js
+++ src/syslog_drain.js
@@ -13,25 +13,25 @@ const messagesBySources = {};
 
 
 const DYNO_STATES = {
-    "crashed": -2,
-    "down": -1,
-    "idle": 0,
-    "starting": 1,
-    "up": 2,
-    "complete": 3
+  "crashed": -2,
+  "down": -1,
+  "idle": 0,
+  "starting": 1,
+  "up": 2,
+  "complete": 3
 };
 
 
 if (process.env.DEBUG_SYSLOG === "true") {
-    log.warn("Syslog log drain is in debug (DEBUG_SYSLOG=true), this may impact memory");
+  log.warn("Syslog log drain is in debug (DEBUG_SYSLOG=true), this may impact memory");
 }
 
 function collect_messages(source, body) {
-    if (process.env.DEBUG_SYSLOG === "true") {
-        const collector = messagesBySources[source] || [];
-        collector.unshift(body);
-        messagesBySources[source] = collector.slice(0, MAX_MESSAGE_SIZE);
-    }
+  if (process.env.DEBUG_SYSLOG === "true") {
+    const collector = messagesBySources[source] || [];
+    collector.unshift(body);
+    messagesBySources[source] = collector.slice(0, MAX_MESSAGE_SIZE);
+  }
 }
 
 /**
@@ -40,15 +40,15 @@ function collect_messages(source, body) {
  * @returns {*}
  */
 function parse_sample_value(value) {
-    const is_numeric = /^[\d.]+$/.test(value);
-    if (!is_numeric) {
-        try {
-            return filesizeParser(value);
-        } catch (ex) {
-            return parseFloat(value);
-        }
+  const is_numeric = /^[\d.]+$/.test(value);
+  if (!is_numeric) {
+    try {
+      return filesizeParser(value);
+    } catch (ex) {
+      return parseFloat(value);
     }
-    return parseFloat(value);
+  }
+  return parseFloat(value);
 }
 exports.parse_sample_value = parse_sample_value;
 
@@ -59,15 +59,15 @@ exports.parse_sample_value = parse_sample_value;
  * @returns {*}
  */
 function parse_duration_value(value) {
+  try {
+    return durationParser(value);
+  } catch (ex) {
     try {
-        return durationParser(value);
+      return parseFloat(value);
     } catch (ex) {
-        try {
-            return parseFloat(value);
-        } catch (ex) {
-            return value;
-        }
+      return value;
     }
+  }
 }
 
 /**
@@ -76,13 +76,13 @@ function parse_duration_value(value) {
  * @returns {[version, user_email]}
  */
 function parse_heroku_release(value) {
-    // Release v16 created by user test@test.com
-    const releaseRegexp = new RegExp(`^Release (v[0-9]+) created by user (.*)$`);
-    const match = releaseRegexp.exec(value);
-    if (!match) {
-        return null;
-    }
-    return [match[1], match[2]];
+  // Release v16 created by user test@test.com
+  const releaseRegexp = new RegExp(`^Release (v[0-9]+) created by user (.*)$`);
+  const match = releaseRegexp.exec(value);
+  if (!match) {
+    return null;
+  }
+  return [match[1], match[2]];
 }
 exports.parse_heroku_release = parse_heroku_release;
 
@@ -93,13 +93,13 @@ exports.parse_heroku_release = parse_heroku_release;
  * @returns {[old_state, new_state]}
  */
 function parse_heroku_state_changed(value) {
-    // State changed from starting to up
-    const releaseRegexp = new RegExp(`^State changed from ([a-zA-Z0-9_]+) to ([a-zA-Z0-9_]+)$`);
-    const match = releaseRegexp.exec(value);
-    if (!match) {
-        return null;
-    }
-    return [match[1], match[2]];
+  // State changed from starting to up
+  const releaseRegexp = new RegExp(`^State changed from ([a-zA-Z0-9_]+) to ([a-zA-Z0-9_]+)$`);
+  const match = releaseRegexp.exec(value);
+  if (!match) {
+    return null;
+  }
+  return [match[1], match[2]];
 }
 exports.parse_heroku_state_changed = parse_heroku_state_changed;
 
@@ -109,18 +109,18 @@ exports.parse_heroku_state_changed = parse_heroku_state_changed;
  * @returns error code
  */
 function parse_heroku_errors(value) {
-    var errorRegexp = new RegExp(`^Error (L|R)([0-9]{2,})`);
-    var match = errorRegexp.exec(value);
+  var errorRegexp = new RegExp(`^Error (L|R)([0-9]{2,})`);
+  var match = errorRegexp.exec(value);
 
-    if (!match) {
-        errorRegexp = new RegExp(`^at=error code=(H)([0-9]{2,})`);
-        match = errorRegexp.exec(value);
-    }
+  if (!match) {
+    errorRegexp = new RegExp(`^at=error code=(H)([0-9]{2,})`);
+    match = errorRegexp.exec(value);
+  }
 
-    if (!match) {
-        return null;
-    }
-    return match[1]+match[2];
+  if (!match) {
+    return null;
+  }
+  return match[1]+match[2];
 }
 
 /**
@@ -130,28 +130,28 @@ function parse_heroku_errors(value) {
  * @returns {Array.<*>}
  */
 function handle_heroku_runtime_metrics(message, tags) {
-    const local_tags = Object.assign({}, tags);
-    return message.message
-        .split(" ").map((item) => {
-            if (item.indexOf("sample#") !== -1) {
-                const [key, value] = item.substring(7).split("=");
-                const metric_name = key.replace(/[\W_]+/g, "_");
-                return {
-                    timestamp: message.time,
-                    name: metric_name,
-                    tags: local_tags,
-                    value: parse_sample_value(value)
-                }
-            } else if (item.indexOf("tag#") !== -1) {
-                // add tag to all samples
-                const [key, value] = item.substring(4).split("=");
-                local_tags[key] = value;
-                return null;
-            } else {
-                return null;
-            }
-        })
-        .filter((i) => i !== null);
+  const local_tags = { ...tags };
+  return message.message
+    .split(" ").map((item) => {
+      if (item.indexOf("sample#") !== -1) {
+        const [key, value] = item.substring(7).split("=");
+        const metric_name = key.replace(/[\W_]+/g, "_");
+        return {
+          timestamp: message.time,
+          name: metric_name,
+          tags,
+          value: parse_sample_value(value)
+        };
+      } else if (item.indexOf("tag#") !== -1) {
+        // add tag to all samples
+        const [key, value] = item.substring(4).split("=");
+        local_tags[key] = value;
+        return null;
+      } else {
+        return null;
+      }
+    })
+    .filter((i) => i !== null);
 }
 exports.handle_heroku_runtime_metrics = handle_heroku_runtime_metrics;
 
@@ -163,84 +163,83 @@ exports.handle_heroku_runtime_metrics = handle_heroku_runtime_metrics;
  * @returns {Array.<*>}
  */
 function handle_heroku_router(message, tags) {
-    const key_values = message.message
-        .split(" ").reduce((acc, item) => {
-            const [key, value] = item.split("=");
-            acc[key] = value;
-            return acc;
-        }, {});
-    const all_tags = Object.assign({
-        method: key_values["method"].toLowerCase(),
-        status: parseInt(key_values["status"]),
-        path: key_values["path"].replace(/"/g, ""),
-        ip: key_values["fwd"].replace(/"/g, "")
-    }, tags);
-    return [{
-        timestamp: message.time,
-        name: "router_access_time",
-        tags: all_tags,
-        fields: {
-            duration: parse_duration_value(key_values["service"]),
-            size: parse_sample_value(key_values["bytes"]),
-            count: 1
-        }
-    }];
+  const key_values = _.fromPairs(
+    message.message
+      .split(" ").
+      map((item) => item.split("="))
+  );
+  return [{
+    timestamp: message.time,
+    name: "router_access_time",
+    tags,
+    fields: {
+      method: key_values["method"].toLowerCase(),
+      status: parseInt(key_values["status"]),
+      path: key_values["path"].replace(/"/g, ""),
+      ip: key_values["fwd"].replace(/"/g, ""),
+      duration: parse_duration_value(key_values["service"]),
+      size: parse_sample_value(key_values["bytes"]),
+      count: 1
+    }
+  }];
 }
 
 
 function handle_heroku_release(message, tags) {
-    const result = parse_heroku_release(message.message);
-    if (!result) {
-        return [];
+  const result = parse_heroku_release(message.message);
+  if (!result) {
+    return [];
+  }
+  return [
+    {
+      timestamp: message.time,
+      name: "heroku_release",
+      fields: {
+        version: result[0],
+        user: result[1]
+      },
+      value: 1,
+      tags,
     }
-    return [
-        {
-            timestamp: message.time,
-            name: "heroku_release",
-            tags: Object.assign({
-                version: result[0],
-                user: result[1]
-            }, tags),
-            value: 1
-        }
-    ]
+  ];
 }
 
 
 function handle_heroku_state_changed(message, tags) {
-    const result = parse_heroku_state_changed(message.message);
-    if (!result) {
-        return [];
+  const result = parse_heroku_state_changed(message.message);
+  if (!result) {
+    return [];
+  }
+  return [
+    {
+      timestamp: message.time,
+      name: "heroku_state",
+      tags: tags,
+      value: DYNO_STATES[result[1]] || -10,
+      fields: {
+        old_state: result[0],
+        new_state: result[1],
+      }
     }
-    return [
-        {
-            timestamp: message.time,
-            name: "heroku_state",
-            tags: tags,
-            value: DYNO_STATES[result[1]] || -10,
-            fields: {
-                old_state: result[0],
-                new_state: result[1],
-            }
-        }
-    ]
+  ];
 }
 
 function handle_heroku_errors(message, tags) {
-    const result = parse_heroku_errors(message.message);
-    if (!result) {
-        return [];
+  const result = parse_heroku_errors(message.message);
+  if (!result) {
+    return [];
+  }
+  return [
+    {
+      timestamp: message.time,
+      name: "heroku_error",
+      tags,
+      fields: {
+        error: result
+      },
+      value: 1
     }
-    return [
-        {
-            timestamp: message.time,
-            name: "heroku_error",
-            tags: Object.assign({
-                error: result
-            }, tags),
-            value: 1
-        }
-    ]
+  ];
 }
 
 /**
@@ -272,37 +271,37 @@ function handle_structlog(message, tags) {
  * @returns Array of influx IPoint
  */
 function message_to_points(message, source, tags={}) {
-    const all_tags = Object.assign({}, tags, {
-        app: message.appName,
-        source: source
-    });
-    if (message.pid) {
-        all_tags["process"] = message.pid.split(".")[0];
-        all_tags["pid"] = message.pid;
+  const all_tags = Object.assign({}, tags, {
+    app: message.appName,
+    source: source
+  });
+  if (message.pid) {
+    all_tags["process"] = message.pid.split(".")[0];
+    all_tags["pid"] = message.pid;
+  }
+  let result = [];
+  if (message.appName === "app") {
+    result = handle_structlog(message, all_tags);
+  } else {
+    if (message.message.indexOf("sample#") !== -1) {
+      result = handle_heroku_runtime_metrics(message, all_tags);
+    } else if (message.message.indexOf("protocol=https") !== -1) {
+      result = handle_heroku_router(message, all_tags);
+    } else if (message.message.indexOf("created by user") !== -1) {
+      result = handle_heroku_release(message, all_tags);
+    } else if (message.message.indexOf("State changed from") !== -1) {
+      result = handle_heroku_state_changed(message, all_tags);
+    } else if ((message.message.indexOf("Error ") === 0) || (message.message.indexOf("at=error code=")) === 0) {
+      result = handle_heroku_errors(message, all_tags);
     }
-    let result = [];
-    if (message.appName === "app") {
-        result = handle_structlog(message, all_tags);
-    } else {
-      if (message.message.indexOf("sample#") !== -1) {
-        result = handle_heroku_runtime_metrics(message, all_tags);
-      } else if (message.message.indexOf("protocol=https") !== -1) {
-        result = handle_heroku_router(message, all_tags);
-      } else if (message.message.indexOf("created by user") !== -1) {
-        result = handle_heroku_release(message, all_tags);
-      } else if (message.message.indexOf("State changed from") !== -1) {
-        result = handle_heroku_state_changed(message, all_tags);
-      } else if ((message.message.indexOf("Error ") === 0) || (message.message.indexOf("at=error code=")) === 0) {
-        result = handle_heroku_errors(message, all_tags);
-      }
+  }
+  // ensure "source" was not changed, by re-setting it.
+  result.forEach((p) => {
+    if (p.tags) {
+      p.tags.source = source;
     }
-    // ensure "source" was not changed, by re-setting it.
-    result.forEach((p) => {
-        if (p.tags) {
-            p.tags.source = source;
-        }
-    });
-    return result;
+  });
+  return result;
 }
 
 
@@ -314,48 +313,48 @@ function message_to_points(message, source, tags={}) {
  * @returns {Promise}
  */
 exports.process_heroku_log = function process_heroku_log(body, source, tags={}) {
-    collect_messages(source, body);
-    return new Promise((resolve) => {
-        const buffer = body;
-        const messages = [];
-        let pos = 0;
-        let new_pos = body.indexOf(" ", pos);
-        while (new_pos !== -1) {
-            const len_str = buffer.substring(pos, new_pos);
-            const len = parseInt(len_str);
-            if (isNaN(len)) {
-                log.debug(`-------- BODY START ------\n${body}\n-------- BODY END ---------\n`);
-                log.error(`Unable to parse message length at position ${pos} text found '${len_str}'`);
-                new_pos = -1;
-                continue;
-            }
-            const message = buffer.substring(new_pos + 1, new_pos + len + 1).trim();
-            messages.push(syslogParser.parse(message));
-            pos = new_pos + len + 1;
-            new_pos = body.indexOf(" ", pos);
-        }
-        log.debug(`messages=${JSON.stringify(messages, null, 4)}`);
-        const points = messages
-            .map((m) => message_to_points(m, source, tags))
-            .filter((p) => p.length > 0)
-            .reduce((a, b) => a.concat(b), []);
-        resolve(points);
-    });
+  collect_messages(source, body);
+  return new Promise((resolve) => {
+    const buffer = body;
+    const messages = [];
+    let pos = 0;
+    let new_pos = body.indexOf(" ", pos);
+    while (new_pos !== -1) {
+      const len_str = buffer.substring(pos, new_pos);
+      const len = parseInt(len_str);
+      if (isNaN(len)) {
+        log.debug(`-------- BODY START ------\n${body}\n-------- BODY END ---------\n`);
+        log.error(`Unable to parse message length at position ${pos} text found '${len_str}'`);
+        new_pos = -1;
+        continue;
+      }
+      const message = buffer.substring(new_pos + 1, new_pos + len + 1).trim();
+      messages.push(syslogParser.parse(message));
+      pos = new_pos + len + 1;
+      new_pos = body.indexOf(" ", pos);
+    }
+    log.debug(`messages=${JSON.stringify(messages, null, 4)}`);
+    const points = messages
+          .map((m) => message_to_points(m, source, tags))
+          .filter((p) => p.length > 0)
+          .reduce((a, b) => a.concat(b), []);
+    resolve(points);
+  });
 };
 
 exports.collector_text = function collector_text(source) {
-    return (messagesBySources[source] || []).join("\n--------------------------------------------------------\n");
+  return (messagesBySources[source] || []).join("\n--------------------------------------------------------\n");
 };
 
 
 exports.collector_index = function collector_index() {
-    if (process.env.DEBUG_SYSLOG !== "true") {
-        return `<html><body><h1>Syslog debug not activated</h1><p>Set DEBUG_SYSLOG=true to enable it.</p></body>`;
-    }
-    const collectors = Object.keys(messagesBySources).map((c) => {
-        return `<li><a href="/_syslog_debug/${c}/">${c}</a></li>`;
-    });
-    return `
+  if (process.env.DEBUG_SYSLOG !== "true") {
+    return `<html><body><h1>Syslog debug not activated</h1><p>Set DEBUG_SYSLOG=true to enable it.</p></body>`;
+  }
+  const collectors = Object.keys(messagesBySources).map((c) => {
+    return `<li><a href="/_syslog_debug/${c}/">${c}</a></li>`;
+  });
+  return `
         <html>
             <body>
                 <h1>Syslog debug collector list</h1>
